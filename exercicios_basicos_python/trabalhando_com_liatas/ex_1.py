e123ea70bfb65dc5d34e58e8c5441586483a8481
igorrm19 <igormachado14@gmail.com>
2025-08-22 04:03:57 -0300

    trabalhando com listas

---
+++ exercicios_basicos_python/trabalhando_com_liatas/ex_1.py
@@ -0,0 +1,12 @@
+#lista com nomes aleatorios
+
+import random;
+
+nomes = ['igor', 'briza', 'tierre'];
+
+random.shuffle(nomes); #deixa os nomes em ordem aleatorio
+
+print(nomes);
+
+for nome in nomes:
+    print(nome);
